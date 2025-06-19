7ef878b71d8287422190443590bdd07671206ef6
Mateus Coelho <teus.hcp@gmail.com>
2025-06-19 19:58:45 -0300

    Initial Commit

---
+++ Filmcity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Filmcity
+//
+//  Created by Mateus Henrique Coelho de Paulo on 19/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FilmcityUITests/FilmcityUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FilmcityUITestsLaunchTests.swift
+//  FilmcityUITests
+//
+//  Created by Mateus Henrique Coelho de Paulo on 19/06/25.
+//
+
+import XCTest
+
+final class FilmcityUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
